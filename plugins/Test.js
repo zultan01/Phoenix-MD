a375fc335439de30db1396d1553999f952b0dc4b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-04-29 15:59:59 +0530

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,3 +1,106 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const {
   Function,
   isPrivate,
@@ -22,17 +125,20 @@ const {
   sleep,
   pnix,
 } = require("../lib/");
+const lib = require("../lib");
 const util = require("util");
 const config = require("../config");
 
 pnix({ pattern: 'eval', on: "text", fromMe: true, desc: 'Runs a server code' }, async (message, match, m, client) => {
+  //
   if (match.startsWith(">")) {
-    try {
-      let evaled = await (async () => eval(`${match.replace(">", "")}`))();
-      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-      await message.reply(evaled);
-    } catch (err) {
-      await message.reply(util.format(err));
-    }
+            try {
+                let evaled = await eval(`(async () => { ${match.replace(">", "")} })()`);
+                if (typeof evaled !== "string") evaled = util.inspect(evaled);
+                await message.reply(`${'```'}${evaled}${'```'}`)
+            } catch (err) {
+                await message.reply(`_${util.format(err)}_`);
+            }
   }
+  //
 });
